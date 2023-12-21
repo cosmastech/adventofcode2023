0d0dfb0f39974a0ef340f04652e54ddb2cb7ed2a
Luke Kuzmish <lakuzmish@gmail.com>
2023-12-21 13:07:19 -0500

    day 15 part 1

--- day15/day15.py
+++ day15/day15.py
@@ -1,10 +1,26 @@
 from input import REAL_INPUT, EXAMPLE_INPUT
 
+def parse_input_str(input_str: str) -> list[str]:
+    return input_str.split(',')
 
+def parse_chunk(chunk_str: str) -> int:
+    value: int = 0
+    for char in chunk_str:
+        value += ord(char)
+        value *= 17
+        value %= 256
+    return value
 def part1(input_str: str) -> int:
-    return 0
+    output: int = 0
+
+    chunks = parse_input_str(input_str)
+    for chunk in chunks:
+        output += parse_chunk(chunk)
+
+    return output
+
 
 
 if __name__ == '__main__':
-    input_str = EXAMPLE_INPUT
+    input_str = REAL_INPUT
     print("Part 1: " + str(part1(input_str)))
