7cc43d4e3887aa3b242ed06fba3a55cc5edeab38
Luke Kuzmish <lakuzmish@gmail.com>
2023-12-21 13:02:27 -0500

    day 15 skeleton

---
+++ day15/day15.py
@@ -0,0 +1,10 @@
+from input import REAL_INPUT, EXAMPLE_INPUT
+
+
+def part1(input_str: str) -> int:
+    return 0
+
+
+if __name__ == '__main__':
+    input_str = EXAMPLE_INPUT
+    print("Part 1: " + str(part1(input_str)))
